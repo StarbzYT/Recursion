99392d4ba0358d6c8e8406a2c3d06563b96ae7c4
Adarsh <chhokaradarsh@gmail.com>
2020-07-26 14:41:21 -0700

    added other functions

--- call_stack_example.py
+++ call_stack_example.py
@@ -1,3 +1,4 @@
+from random import choice
 # for function that calls othe funcs, python uses the callstack to manage the order of which functions should run
 # think of the callstack like a stack of papers, you add to the top and remove from the top. (dev tools include a call stack)
 # the latest function called is added to the top of the stack and then removed once python reaches the return keyword.
@@ -15,9 +16,17 @@ def eat_breakfast():
 
 
 def cook_food():
+    items = ["eggs", "toast", "waffles", "bacon", "sausages"]
+    return choice(items)
 
 
+def wake_up():
+    # calls all other funcs
+    showering()
+    eat_breakfast()
+    print("Okay, ready for work!")
 
 
+wake_up()
 
 
